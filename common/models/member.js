d5cab46288fed10c4f20ec227da8dac218c881d3
David Connelly Mac11 <david.webguy@gmail.com>
2017-12-04 18:06:45 +0000

    end of lesson 8 - remote methods

--- common/models/member.js
+++ common/models/member.js
@@ -2,4 +2,9 @@
 
 module.exports = function(Member) {
 
+    Member.sayMyName = function(myName, callback) {
+      var myResponse = 'Your name is ' + myName;
+      callback(null, myResponse);
+    };
+
 };
