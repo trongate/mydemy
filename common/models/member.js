d6eeddcdbb8038a62fda4fec72e626c99586ba0c
David Connelly Mac11 <david.webguy@gmail.com>
2017-12-05 23:20:26 +0000

    end of lesson 10:  now have simple example of both before and after remote hooks working

--- common/models/member.js
+++ common/models/member.js
@@ -6,11 +6,13 @@ module.exports = function(Member) {
       callback(null, firstName);
     };
 
+    //simple example of a before remote hook
     Member.beforeRemote('sayMyName', function(context, unused, next) {
         console.log('Putting in the car key, starting the engine.');
         next();
     });
 
+    //simple example of an after remote hook
     Member.afterRemote('sayMyName', function(context, finalOutput, next) {
         console.log('This is the after remote hook running.');
         next();
